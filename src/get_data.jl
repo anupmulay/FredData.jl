003667bd0802e0c25aff50b8e409f84d2a367dc1
Alfred Duncan <a.j.m.duncan@kent.ac.uk>
2019-08-16 17:47:16 +0100

    function fixes

--- src/get_data.jl
+++ src/get_data.jl
@@ -191,16 +191,16 @@ end
 function get_as_dataframe(f::Fred, series::AbstractString)
     fred_dat = get_data(f, series)
     df       = fred_dat.data[:,[:date,:value]]
-    rename!(df,:value => Symbol("series"))
+    rename!(df,:value => Symbol(series))
     return df
 end
 
-function get_panel(f::Fred, Series::Array{AbstractString,1})
-    df = get_as_dataframe(f, Series[1])
+function get_panel(f::Fred, Series::Array{String,1})
+    df = get_as_dataframe(f, AbstractString(Series[1]))
     if size(Series)[1] > 1
         for i in 2:size(Series)[1]
             df = join(df,
-                      get_as_dataframe(f, Series[i]),
+                      get_as_dataframe(f, AbstractString(Series[i])),
                       on = :date)
         end
     end
